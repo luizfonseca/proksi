4e4c989f4407f15ec91cd178b8355db29655fa61
Luiz Fonseca <eu@luiz.cc>
2024-05-14 01:20:16 +0200

    fix(goreleaser): attempt on fixing the broken build process

--- build-cross-release.sh
+++ build-cross-release.sh
@@ -1,10 +1,8 @@
-cargo install cross --git https://github.com/cross-rs/cross
-
 cross build --release --target=x86_64-unknown-linux-gnu
 cross build --release --target=x86_64-apple-darwin
 
 mkdir -p dist/proksi_darwin_amd64_v1
 mkdir -p dist/proksi_linux_amd64_v1
 
-cp target/x86_64-apple-darwin/release/proksi dist/proksi_darwin_amd64
+cp target/x86_64-apple-darwin/release/proksi dist/proksi_darwin_amd64_v1
 cp target/x86_64-unknown-linux-gnu/release/proksi dist/proksi_linux_amd64_v1
