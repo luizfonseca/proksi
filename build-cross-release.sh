be17f634158fe035148bb76a6611ae207d10c773
Luiz Fonseca <eu@luiz.cc>
2024-05-13 23:05:36 +0200

    chore(goreleaser): update post hook script

--- build-cross-release.sh
+++ build-cross-release.sh
@@ -2,8 +2,8 @@
 # rustup target add x86_64-unknown-linux-gnu
 # rustup target add aarch64-apple-darwin
 
-docker run --rm -v `pwd`:/app rust:slim-buster sh -c 'cd /app && cargo build --release --target=x86_64-unknown-linux-gnu'
-cargo build --release --target=aarch64-apple-darwin
+docker run --rm -v `pwd`:/app rust:slim-buster sh -c 'cd /app rustup target x86_64-unknown-linux-gnu && cargo build --release --target=x86_64-unknown-linux-gnu --bin proksi'
+cargo build --release --target=aarch64-apple-darwin --bin proksi
 
 mkdir -p dist/darwin_amd64 && cp target/x86_64-apple-darwin/release/proksi dist/darwin_amd64
 mkdir -p dist/linux_amd64 && cp target/x86_64-unknown-linux-gnu/release/proksi dist/linux_amd64
