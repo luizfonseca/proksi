8c72b340dab1ae8db2f3b00214a67f58a04b00ca
Luiz Fonseca <eu@luiz.cc>
2024-06-19 01:54:09 +0200

    chore: clippy

--- src/config/hcl.rs
+++ src/config/hcl.rs
@@ -1,17 +1,18 @@
-use std::io::Read;
+use std::{io::Read, path::Path};
 
 use hcl::{
     eval::{Context, FuncArgs},
     Value,
 };
 
+#[allow(clippy::module_name_repetitions)]
 pub struct HclFormat;
 impl figment::providers::Format for HclFormat {
     type Error = hcl::Error;
 
     const NAME: &'static str = "HCL";
 
-    fn from_str<'de, T: serde::de::DeserializeOwned>(string: &'de str) -> Result<T, Self::Error> {
+    fn from_str<T: serde::de::DeserializeOwned>(string: &str) -> Result<T, Self::Error> {
         hcl::eval::from_str(string, &get_hcl_context())
     }
 }
@@ -23,14 +24,21 @@ impl figment::providers::Format for HclFormat {
 /// // HCL document
 /// config = read_file("config.toml")
 /// ```
+#[allow(clippy::needless_pass_by_value)]
 fn read_hcl_file(args: FuncArgs) -> Result<Value, String> {
-    let path = args[0].as_str().unwrap();
-    if !path.ends_with(".hcl") {
-        return Err(format!("File must be a HCL file: {}", path));
+    let path = Path::new(args[0].as_str().unwrap());
+    if path
+        .extension()
+        .map_or(false, |ext| ext.eq_ignore_ascii_case("hcl"))
+    {
+        return Err(format!(
+            "File must be a HCL file: {}",
+            path.to_string_lossy()
+        ));
     }
 
     let Ok(mut file) = std::fs::File::open(path) else {
-        return Err(format!("file not found: {}", path));
+        return Err(format!("file not found: {}", path.to_string_lossy()));
     };
 
     let mut contents = String::new();
@@ -49,10 +57,11 @@ fn read_hcl_file(args: FuncArgs) -> Result<Value, String> {
 /// host = env("HOST")
 /// port = env("PORT")
 /// ```
+#[allow(clippy::needless_pass_by_value)]
 fn get_env_var(args: FuncArgs) -> Result<Value, String> {
     let key = args[0].as_str().unwrap();
     let Ok(var) = std::env::var(key) else {
-        return Err(format!("Environment variable {} not found", key));
+        return Err(format!("Environment variable {key} not found"));
     };
 
     Ok(Value::String(var))
@@ -75,5 +84,5 @@ fn get_hcl_context<'a>() -> Context<'a> {
     context
 }
 
-/// HashiCorp Configuration Language (HCL) provider for figment
+/// hashicorp Configuration Language (HCL) provider for figment
 pub type Hcl = figment::providers::Data<HclFormat>;
