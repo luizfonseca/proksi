256e552c6df3971955712c1326c12a9f0f61996f
Luiz Fonseca <eu@luiz.cc>
2025-04-11 02:22:47 +0200

    feat: add global store

---
+++ crates/proksi/src/stores/global.rs
@@ -0,0 +1,16 @@
+use once_cell::sync::OnceCell;
+use std::sync::Arc;
+
+use super::adapter::Store;
+
+static GLOBAL_STORE: OnceCell<Arc<dyn Store>> = OnceCell::new();
+
+pub fn init_store<S: Store>(store: S) {
+    if GLOBAL_STORE.set(Arc::new(store)).is_err() {
+        panic!("Global store already initialized");
+    }
+}
+
+pub fn get_store() -> &'static Arc<dyn Store> {
+    GLOBAL_STORE.get().expect("Global store not initialized")
+}
